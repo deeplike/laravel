7ed38fa7f4b590202a80a9af31289d66f0513698
Ren Hao <wetyped@gmail.com>
2013-06-18 22:01:59 +0800

    add avatar files

---
+++ app/views/account/avatar.blade.php
@@ -0,0 +1,30 @@
+<?php
+$user = Auth::user();
+?>
+
+    @section('title')
+    修改头像
+    @stop
+
+    <h4>上传头像</h4>
+
+<?php if($errors->has('avatar')): ?>
+    <?php var_dump($errors->first('avatar')); ?>
+<?php endif; ?>
+
+    <div class="">
+        {{HTML::image($user->avatar ? $user->avatar : URL::asset('images/default_avatar.jpg'), $user->name, array('class'=>'img-rounded'))}}
+    </div>
+
+<?php echo Form::open(array('files'=>true)); ?>
+
+    <div class="mt10">
+        <?php echo Form::file('avatar', array('title'=>'选择头像')) ?>
+    </div>
+
+    <div class="form-actions">
+        <button type="submit" class="btn btn-primary">保存</button>
+        <button type="button" class="btn">取消</button>
+    </div>
+
+<?php Form::close(); ?>
